8ea925f901930545895b6771ced089de2fd850bf
Luigi Sacco <luigi.sa94@gmail.com>
2022-08-17 17:04:03 +0200

    Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,22 @@
 // TODOS:
 // Arrows navigation in suggestions list
 
+import '../css/style.scss'
+import logo from '../img/logo.png'
+import favicon from '../img/favicon.png'
 import { getSuggestions } from "./getSuggestions.js"
 import { onSubmit } from "./onSubmit.js"
 
 const button = document.querySelector('#search-button')
 const input = document.querySelector("#search-input")
 const label = document.querySelector(".search__label") 
+const logoImg = document.querySelector("#logo") 
+const faviconElem = document.querySelector("link[rel~='icon']");
 
+faviconElem.href = favicon
+logoImg.src = logo
+
+// google style placeholer
 input.addEventListener('focus', () => {
     label.classList.add("label-up")
 })
@@ -15,6 +24,7 @@ input.addEventListener('focusout', () => {
     if (input.value == '') label.classList.remove("label-up")
 })
 
+// if enter key pressed click the btn
 input.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
@@ -22,11 +32,15 @@ input.addEventListener("keypress", (e) => {
     }
 });
 
+// if btn clicked, submit the input
 button.addEventListener('click', (e) => {
     e.preventDefault()
     onSubmit() 
 })
 
+// suggest cities db has
 input.addEventListener('input', (e) => {
     getSuggestions()
 })
+
+
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]'
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: 5000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
